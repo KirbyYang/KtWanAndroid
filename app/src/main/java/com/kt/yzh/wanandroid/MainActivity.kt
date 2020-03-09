cea0f07bd5cfc585921a7d1e07094ed2ab907027
kirbyyang951 <727987455@qq.com>
2020-03-09 22:20:44 +0800

    Initial commit

---
+++ app/src/main/java/com/kt/yzh/wanandroid/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.kt.yzh.wanandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setHello()
+    }
+
+    private fun setHello(){
+        tvHello.text = "Hello World!"
+    }
+}
